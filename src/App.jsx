2b62d91035d5d7c109fc68f314c48c4e0458c21e
raveena69 <raveena1996@gmail.com>
2021-04-22 20:10:40 +0530

    Initial Commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import React, {Component} from 'react';
+import Home from './Home';
+
+export default class App extends React.Component{
+
+    render() {
+
+        return(
+            <div>
+                <Home />
+            </div>
+        );
+    }
+};
---
+++ src/Home.jsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import './css/Home.css';
+
+export default class Home extends React.Component {
+
+    render() {
+        return(
+            <div>
+                <div className="out-background">
+                    <div className="top-bar">
+                    <div className="top-bar1">
+                        {/*<img className="logo" src="./images/logo1.png"/>*/}
+                        <div className="conference-name">International Conference on Application Frameworks – ICAF</div>
+                    </div>
+                    </div>
+                </div>
+
+            </div>
+        );
+    }
+};
